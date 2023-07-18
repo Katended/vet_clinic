3bb4e69800c9f4ff3f3f844d02fba08b5e48c694
David Katende <katendedavidjoga@gmail.com>
2023-07-18 11:11:17 +0300

    Add queries

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
-/* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals  (
+   id INT NOT NULL,
+   name VARCHAR (100),
+   date_of_birth DATE,
+   escape_attempts INT NOT NULL,
+   neutered BOOLEAN,
+   weight_kg DECIMAL (18, 2)   
 );
