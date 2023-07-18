7581ac114d525c617f4bdf15775783afaa31443f
David Katende <katendedavidjoga@gmail.com>
2023-07-18 10:48:44 +0300

    Create database

--- schema.sql
+++ schema.sql
@@ -1,8 +1,5 @@
-CREATE TABLE animals  (
-   id INT NOT NULL,
-   name VARCHAR (100),
-   date_of_birth DATE,
-   escape_attempts INT NOT NULL,
-   neutered BOOLEAN,
-   weight_kg DECIMAL (18, 2)   
+/* Database schema to keep the structure of entire database. */
+
+CREATE TABLE animals (
+    name varchar(100)
 );
