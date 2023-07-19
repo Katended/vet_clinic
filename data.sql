6125a27fd0db0b71cad32e6b14f4c6e2a777682f
David Katende <katendedavidjoga@gmail.com>
2023-07-19 18:19:02 +0300

    Query mulile tables

--- data.sql
+++ data.sql
@@ -22,3 +22,23 @@ INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES (10, 'Blossom', '1998-11-13', 3,TRUE,17);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES (11, 'Ditto', '2022-05-14', 4,TRUE,22);
+
+-- insert data into owners
+INSERT INTO owners (full_name,age)
+VALUES ('Sam Smith', '34');
+INSERT INTO owners (full_name,age)
+VALUES ('Jennifer Orwell', '19');
+INSERT INTO owners (full_name,age)
+VALUES ('Bob', '45');
+INSERT INTO owners (full_name,age)
+VALUES ('Dean Winchester', '14');
+INSERT INTO owners (full_name,age)
+VALUES ('Melody Pond', '77');
+INSERT INTO owners (full_name,age)
+VALUES ('Jodie Whiataker', '38');
+
+-- insert data into species
+INSERT INTO species (name)
+VALUES ('Pokemon');
+INSERT INTO species (name)
+VALUES ('Digimon');
--- queries.sql
+++ queries.sql
@@ -56,3 +56,49 @@ select species,MAX(weight_kg),MIN(weight_kg)  from animals GROUP BY species;
 -- What is the average number of escape attempts per animal type of those born between 1990 and 2000
 select sum(escape_attempts)/count(id)  from animals
 WHERE EXTRACT(YEAR from date_of_birth) BETWEEN 1990 and 2000;
+
+
+-- Modify your inserted animals so it includes the species_id
+UPDATE animals set species_id = (SELECT id from species WHERE name='Digimon') WHERE name LIKE '%mon'
+
+-- Modify your inserted animals to include owner information
+UPDATE animals set owner_id = 
+(select id FROM owners where full_name='Sam Smith') WHERE name='Agumon';
+UPDATE animals set owner_id = 
+(select id FROM owners where full_name='Jennifer Orwell') WHERE name='Pikachu' or name='Gabumon';
+UPDATE animals set owner_id = 
+(select id FROM owners where full_name='Bob') WHERE name='Plantmon' or name='Devimon';
+UPDATE animals set owner_id = 
+(select id FROM owners where full_name='Melody Pond') WHERE name='Squirtle' or name='Blossom' or name='Charmander';
+UPDATE animals set owner_id = 
+(select id FROM owners where full_name='Dean Winchester') WHERE name='Angemon' or name ='Boarmon';
+
+-- What animals belong to Melody Pond?
+select * from animals inner join owners on animals.owner_id=owners.id where full_name='Melody Pond';
+
+-- List of all animals that are pokemon (their type is Pokemon).
+select * from animals inner join species on animals.species_id=species.id where species.name='Pokemon';
+
+-- List all owners and their animals, remember to include those that don't own any animal
+select * from owners left outer join animals on owners.id=animals.owner_id;
+
+-- How many animals are there per species?
+select species.name,count(animals.species_id) from animals inner join species on animals.species_id=species.id group by species.id
+
+-- List all Digimon owned by Jennifer Orwell.
+select animals.* from owners , species,animals 
+WHERE animals.species_id=species.id 
+and animals.owner_id=owners.id  
+and full_name='Jennifer Orwell' 
+AND species.name='Digimon';
+
+-- List all animals owned by Dean Winchester that haven't tried to escape.
+select animals.* from owners , animals where
+ animals.owner_id=owners.id  
+and full_name='Dean Winchester' 
+AND animals.escape_attempts = 0;
+
+-- Who owns the most animals
+select owners.*,aa.count FROM (select owner_id,count(id) count from animals group by owner_id) as aa , owners
+where aa.owner_id =owners.id and count =(select max(bb.count2) from (select owner_id,count(id) count2 from animals group by owner_id)as bb)
+
